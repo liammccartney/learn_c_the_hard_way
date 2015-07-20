2c25dd6957c49a7059dc2d872447f3146e4224cd
Liam McCartney <liam.mccartney@gmail.com>
2015-07-20 17:39:11 -0400

    changes some lines as a refresher

--- ex01/ex1.c
+++ ex01/ex1.c
@@ -1,8 +1,9 @@
-# include <stdio.h>
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
-	puts("Hello world.");
-	puts("This is a C Program.");
-	puts("Yes it is");
-	return 0;
+    puts("Hello world.");
+    puts("I am learning C");
+    puts("I used to know C, but I forgot everything, that was 9 years ago");
+
+    return 0;
 }
