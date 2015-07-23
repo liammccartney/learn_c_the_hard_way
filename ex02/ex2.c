fbb2a4d994ceac0f365984ffda987791a603afa8
Liam McCartney <liam.mccartney@gmail.com>
2015-07-23 17:21:43 -0400

    cps ex1 to ex2

---
+++ ex02/ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("I am learning C");
+    puts("I used to know C, but I forgot everything, that was 9 years ago");
+
+    return 0;
+}
