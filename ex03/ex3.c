af629a17c00b6e7d275f69fbbf46aed0dcad046c
Liam McCartney <liam.mccartney@gmail.com>
2015-07-27 20:13:22 -0400

    rewrites ex3

--- ex03/ex3.c
+++ ex03/ex3.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-	int age = 10;
-	int height = 72;
-	printf("I am %d years old.\n", age);
-	printf("I am %d inches tall.\n", height);
+    int age = 10;
+    int height = 72;
 
-	return 0;
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
 }
