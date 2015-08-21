ee09ed4926da88a13f3dbe30c6cdf39504bb8cdf
Liam McCartney <liam.mccartney@gmail.com>
2015-08-21 18:58:57 -0400

    fixes intentionally wrong file

--- ex04/ex4.c
+++ ex04/ex4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int age = 10;
-    int height;
+    int age = 26;
+    int height = 72;
 
-    printf("I am %d years old.\n");
+    printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
 
     return 0;
